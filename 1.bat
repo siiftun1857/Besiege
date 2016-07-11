46338f7e1da48a750e39237b9a2f373a51a8a987
Mike "siiftun1857" Xaiwelles XU <39226927@qq.com>
2016-07-11 08:33:20 +0800

    Delete 1.bat

--- 1.bat
+++
@@ -1,4 +0,0 @@
-raw @echo off
-echo 0.0.0.0	www.bilibili.tv>>%windir%\system32\drivers\etc\hosts
-echo 0.0.0.0	www.bilibili.com>>%windir%\system32\drivers\etc\hosts
-echo 0.0.0.0	www.youku.com>>%windir%\system32\drivers\etc\hosts
